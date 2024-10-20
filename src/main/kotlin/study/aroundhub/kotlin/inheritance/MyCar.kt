f73f2e2e79a39e2779602eacb7e22cbc1cdeb5e3
jisuyoun <jisuyoun@gmail.com>
2024-10-21 08:19:16 +0900

    상속

---
+++ src/main/kotlin/study/aroundhub/kotlin/inheritance/MyCar.kt
@@ -0,0 +1,11 @@
+package org.example.study.aroundhub.kotlin.inheritance
+
+import java.time.LocalDate
+
+class MyCar(name: String, price: Double, brand: String, private val purchaseDate: LocalDate)
+: Car(name, price, brand) {
+	override fun myPurchaseDate() {
+		println("you made a purchase on $purchaseDate")
+	}
+}
+
